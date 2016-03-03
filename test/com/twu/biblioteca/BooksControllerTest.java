12373bf7da02ff99a2c39cf0bda9471b88ba5d17
Ritabrata-TW <rmoitra@thoughtworks.com>
2016-03-03 19:49:56 +0530

    Tests added

--- test/com/twu/biblioteca/BooksControllerTest.java
+++ test/com/twu/biblioteca/BooksControllerTest.java
@@ -3,12 +3,16 @@ package com.twu.biblioteca;
 import com.twu.biblioteca.Controller.BooksController;
 import com.twu.biblioteca.Model.Book;
 import com.twu.biblioteca.Model.Books;
+import com.twu.biblioteca.Model.InvalidInputException;
+import com.twu.biblioteca.Model.NotFoundException;
 import com.twu.biblioteca.View.AppView;
 import com.twu.biblioteca.View.BooksView;
+import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static org.mockito.Mockito.*;
 
@@ -26,7 +30,7 @@ public class BooksControllerTest {
         book = new Book(101, "Head First Design Pattern!", "Martin Fowler", 2007, false);
         books = new ArrayList<Book>(5);
         books.add(book);
-        booksModel = new Books(books);
+        booksModel = mock(Books.class);
         booksView = mock(BooksView.class);
         appView = mock(AppView.class);
         booksController = new BooksController(booksModel, booksView, appView);
@@ -35,6 +39,7 @@ public class BooksControllerTest {
     @Test
     public void shouldBeAbleToDisplayBooks() {
         doNothing().when(booksView).displayBooks(books);
+        when(booksModel.getBooks()).thenReturn(books);
 
         booksController.displayBooks();
 
@@ -55,5 +60,31 @@ public class BooksControllerTest {
         booksController.returnBook(101);
 
         verify(appView).displayMessage("Thank you! Enjoy the book! ");
+    }
+
+    @Test
+    public void shouldBeAbleToWarnUserIfInputIsInvalid() throws NotFoundException, InvalidInputException {
+        when(booksModel.checkoutBook(1)).thenThrow(new InvalidInputException());
+
+        booksController.checkoutBook(1);
+
+        verify(appView).displayMessage("Please select a valid option! ");
+    }
+
+    @Test
+    public void shouldBeAbleToWarnUserIfBookDoesNotExist() throws NotFoundException, InvalidInputException {
+        when(booksModel.checkoutBook(1)).thenThrow(new NotFoundException("Book not found!"));
+
+        booksController.checkoutBook(1);
+
+        verify(appView).displayMessage("That book is not available.");
+
+    }
+
+    @Test
+    public void shouldBeAbleToGetBookNumber() {
+        when(booksView.getBookNumber("Enter the number of the book. ")).thenReturn(1);
+
+        Assert.assertEquals(1,booksController.getBookNumber("Enter the number of the book. "),0);
     }
 }
